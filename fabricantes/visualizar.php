72598f64d5d1de8ddf5a7c91f5d866e59969543d
vitor.gama <vitorggam@hotmail.com>
2023-08-24 08:55:54 -0300

    Sistemas Vendas - CRUD PHP e SQL

---
+++ fabricantes/visualizar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - visualização</title>
+</head>
+<body>
+    <h1>Fabricantes | SELECT -</h1>
+    <a href="../index.php">Home</a>
+    <hr>
+    <h2>Lendo e carregando todos os fabricantes.</h2>
+
+    <a href="inserir.php">Inserir novo fabricante</a>
+
+</body>
+</html>
