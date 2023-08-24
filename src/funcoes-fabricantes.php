f9f5a7d569976a11805a8e3d8ea037f5406b448f
vitor.gama <vitorggam@hotmail.com>
2023-08-24 10:51:39 -0300

    uso do prepare, execute e fetchALL

---
+++ src/funcoes-fabricantes.php
@@ -0,0 +1,35 @@
+<?php
+/*Importando o script de conexão 
+require_once garante que a importação 
+é feita somente uma vez*/
+require_once "conecta.php";
+
+//usada em fabricantes/visualizar.php
+function lerFabricantes( PDO $conexao ){
+    $sql = "SELECT * FROM fabricantes ORDER BY nome";
+
+    try {/*Método prepare():
+    Faz uma preparação/pré-config do comando sql e guarda em memória 
+    (variavel consulta).*/
+    $consulta = $conexao->prepare($sql);
+
+    /*método execute():
+    Executa o comando SQL no banco de dados*/
+    $consulta->execute(); 
+
+      /*Método fetchALL()
+    Buscar todos os dados da consulta como uma array assosciativo*/
+    $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        //code...
+    } catch (Exception $erro) {
+        die("erro".$erro->getMessage());
+        
+    }     
+
+    return $resultado;
+
+}
+
+//teste 
+$dados = lerFabricantes($conexao);
+var_dump($dados);
